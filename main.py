00c2d6babf444da5748d13d1530ba3c273e3476c
LukkeWal <lukke01@yahoo.nl>
2024-12-04 15:00:31 +0100

    fixed error in main

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import time
 
 from PrepareData import load_and_save_raw_data, load_precomputed_data
-from DataStatistics import generate_and_visualize_stats, plot_time_series
+from DataStatistics import generate_and_visualize_stats
 
 
 def main():
     start_time = time.time()
     #data = load_and_save_raw_data("LADPU", "time_series_matrix.csv")
     data = load_precomputed_data("time_series_matrix_TESTING.csv")
-    plot_time_series(data)
     end_time = time.time()
     elapsed_time = end_time - start_time
     print(f"loaded data in {elapsed_time:.4f} seconds")
