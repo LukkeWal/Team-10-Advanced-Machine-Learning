afc9da00c5d57dcdbf2d726160630e2053939c47
LukkeWal <lukke01@yahoo.nl>
2024-12-04 14:58:30 +0100

    removed incorrect time series matrix

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import time
 
 from PrepareData import load_and_save_raw_data, load_precomputed_data
-from DataStatistics import generate_and_visualize_stats
+from DataStatistics import generate_and_visualize_stats, plot_time_series
 
 
 def main():
     start_time = time.time()
-    data = load_and_save_raw_data("LADPUTESTING", "time_series_matrix_TESTING.csv")
+    #data = load_and_save_raw_data("LADPU", "time_series_matrix.csv")
+    data = load_precomputed_data("time_series_matrix_TESTING.csv")
+    plot_time_series(data)
     end_time = time.time()
     elapsed_time = end_time - start_time
-    print(f"Execution time: {elapsed_time:.4f} seconds")
-    generate_and_visualize_stats(data)
+    print(f"loaded data in {elapsed_time:.4f} seconds")
+    #generate_and_visualize_stats(data)
     return
 
 if __name__ == "__main__":
