40dc10e5f5821408c738ae71730543e56406a826
Andrei Secuiu <a.secuiu@student.rug.nl>
2025-01-14 17:04:44 +0100

    Started regressing
    - Created new folder with a test set of users
    - Created a function XGBoost_global()
    - Did global/per user regression using: linear regression, prevWeek, XGBoost
    - Plotted the residues, saw the performance

--- XGBoost.py
+++ XGBoost.py
@@ -1,5 +1,7 @@
 from xgboost import XGBRegressor
 import numpy as np
+import pandas as pd
+
 from sklearn.metrics import root_mean_squared_error
 
 ##### Code examples: Use XGBoost and plot #####
@@ -27,7 +29,7 @@ from sklearn.metrics import root_mean_squared_error
 
 ##### Main functions #####
 
-def XgBoost_meter(X_train, X_test, y_train, y_test):
+def XgBoost_meter(X_train, X_test, y_train, y_test, ):
 
     model = XGBRegressor() # specify paramters
     model.fit(X_train, y_train)
@@ -37,3 +39,58 @@ def XgBoost_meter(X_train, X_test, y_train, y_test):
     scores = [RMSE]
 
     return y_pred, scores
+
+def XgBoost_global(data_meters: list[pd.DataFrame],
+                   split_horizontal = False, 
+                   size = 0.8):
+
+    """
+    This function applies XGBoost to the data for all users (globally) in the following way: data frames
+    are concatenated vertically, so essentially we mix all 9-tuples together. The format is the one returned by 
+    construct_feature_matrix_DataFrame() (so after applying the sliding window, with 9-tuples as rows).
+
+    The return will be a tuple with:
+    - an array with the predicted values for the peak value.
+    - the true values to be predicted
+
+    --- Other inputs:
+
+    split_horizontal = Boolean. If true, the function splits the data such that only on the first (size * len(data_meters)) **users**
+    are used for training (but with their whole time series). The rest of the users are used for predictions
+
+    size = Float. The fraction of users used for training the data
+    """
+
+    if split_horizontal == False:
+        # Concatenate all in one DataFrame
+        data = pd.concat(data_meters, ignore_index=True, axis=0)
+
+        # Extract relevant values as arrays
+        y = data["Peak Value"].values
+        X = data.drop(columns=["Peak Value", "Peak Position"]).values
+
+        # Fit XGBoost
+        model = XGBRegressor() # specify paramters
+        model.fit(X, y)
+        y_pred = model.predict(X)
+
+        return y_pred, y
+    
+    else:
+        # Keep the first users only
+        data_train = pd.concat(data_meters[:int(size * len(data_meters))], axis=0, ignore_index=True)
+        data_test = pd.concat(data_meters[int(size * len(data_meters)):], axis=0, ignore_index=True)
+
+        # Extract relevant values as arrays
+        y_train = data_train["Peak Value"].values
+        X_train = data_train.drop(columns=["Peak Value", "Peak Position"]).values
+
+        y_test = data_test["Peak Value"].values
+        X_test = data_test.drop(columns=["Peak Value", "Peak Position"]).values
+
+        # Fit XGBoost
+        model = XGBRegressor() # specify paramters
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+
+        return y_pred, y_test
--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from sklearn.metrics import root_mean_squared_error
-from PrepareData import read_data
+from PrepareData import read_data, prev_week_DataFrame
 from LinearRegression import linregress_meter, linregress_global
-from XGBoost import XgBoost_meter
+from XGBoost import XgBoost_meter, XgBoost_global
 
 from pdb import set_trace
 
@@ -20,7 +20,156 @@ def main():
     elapsed_time = end_time - start_time
     print(f"processed data in {elapsed_time:.4f} seconds")
 
-    ##### the rest... #####
+    ##### Start with the grid search #####
+
+    # For simplicitly, let's start with a single user, and develop the machinery on it.
+
+    # --- The baseline: linear regression ---
+
+    ## Start with a single user
+
+    # user = 5
+    # data_user = data[user]
+
+    # y_pred, y_true = linregress_meter(data_user, split_vertical=True, size=0.8)
+    # print(f"RMSE: {root_mean_squared_error(y_pred, y_true)}")
+
+    # # Plot the predictions
+    # fig1 = plt.figure(figsize=(6, 6))
+
+    # plt.plot(y_true, color="blue", label="True")
+    # plt.plot(y_pred, color="red", label="Prediction")
+
+    # plt.title(f"Meter {user} - local prediction")
+    # plt.legend()
+    # plt.show()
+
+    # # Plot the residues
+    # fig2 = plt.figure(figsize=(6, 6))
+
+    # plt.plot(y_true - y_pred, color="blue", label="True")
+
+    # plt.title(f"Meter {user} - residues")
+    # plt.legend()
+    # plt.show()
+
+    # fig3 = plt.figure(figsize=(6, 6))
+
+    # plt.hist(y_true - y_pred, color="blue", label="True", bins=100)
+
+    # plt.title(f"Meter {user} - residues")
+    # plt.legend()
+    # plt.show()
+
+    ## What if we have more users? And compared to prevweek?
+
+    # residue_list = np.array([])
+    # residue_list_prev = np.array([])
+
+    # for data_user in data:
+    #     y_pred, y_true = linregress_meter(data_user, split_vertical=True, size=0.8)
+    #     y_pred_prev, _ = prev_week_DataFrame(data_user)
+        
+    #     residue_list = np.concatenate((residue_list, y_true - y_pred))
+    #     residue_list_prev = np.concatenate((residue_list_prev, y_true - y_pred_prev[-y_true.size:]))
+    
+    # print(residue_list.mean())
+    # print(residue_list.std())
+
+    # print(residue_list_prev.mean())
+    # print(residue_list_prev.std())
+
+    # # Intermediate results:
+
+    # # PrevWeek: mean 0.02, std 1.06
+    # # LinRegress: mean 0.04, std 0.9
+
+    # fig3 = plt.figure(figsize=(6, 6))
+
+    # plt.hist(residue_list, color="blue", label="LinearRegression", 
+    #          bins=100, density=True, alpha=0.8)
+    # plt.hist(residue_list_prev, color="red", label="PrevWeek", 
+    #          bins=100, density=True, alpha = 0.8)
+
+    # plt.title(f"Pool all users - residues")
+    # plt.legend()
+    # plt.show()
+
+    ## --- Do global prediction with linear regression
+
+    # y_pred, y_true = linregress_global(data, split_horizontal=True, size=0.8)
+    # residues = y_pred - y_true
+    
+    # print(f"RMSE: {root_mean_squared_error(y_pred, y_true)}")
+    # print(residues.mean())
+    # print(residues.std())
+
+    # # Intermediate result: mean 0.0008, std 0.9
+
+    # # Plot the predictions
+    # fig1 = plt.figure(figsize=(6, 6))
+
+    # plt.plot(y_true, color="blue", label="True")
+    # plt.plot(y_pred, color="red", label="Prediction")
+
+    # plt.title(f"Global prediction (test set)")
+    # plt.legend()
+    # plt.show()
+
+    # # Plot the residues
+    # fig2 = plt.figure(figsize=(6, 6))
+
+    # plt.plot(residues, color="blue", label="True")
+
+    # plt.title(f"Global prediction (test set) - residues")
+    # plt.legend()
+    # plt.show()
+
+    # fig3 = plt.figure(figsize=(6, 6))
+
+    # plt.hist(residues, color="blue", label="True", bins=100)
+
+    # plt.title(f"Global prediction (test set) - residues")
+    # plt.legend()
+    # plt.show()
+
+    ## --- Do some simple XGBoost regression - global prediction
+
+    # y_pred, y_true = XgBoost_global(data, split_horizontal=True, size=0.8)
+    # residues = y_pred - y_true
+    
+    # print(f"RMSE: {root_mean_squared_error(y_pred, y_true)}")
+    # print(residues.mean())
+    # print(residues.std())
+
+    # # Intermediate result: mean -0.002, std 0.9
+
+    # # Plot the predictions
+    # fig1 = plt.figure(figsize=(6, 6))
+
+    # plt.plot(y_true, color="blue", label="True")
+    # plt.plot(y_pred, color="red", label="Prediction")
+
+    # plt.title(f"Global prediction (test set)")
+    # plt.legend()
+    # plt.show()
+
+    # # Plot the residues
+    # fig2 = plt.figure(figsize=(6, 6))
+
+    # plt.plot(residues, color="blue", label="True")
+
+    # plt.title(f"Global prediction (test set) - residues")
+    # plt.legend()
+    # plt.show()
+
+    # fig3 = plt.figure(figsize=(6, 6))
+
+    # plt.hist(residues, color="blue", label="True", bins=100)
+
+    # plt.title(f"Global prediction (test set) - residues")
+    # plt.legend()
+    # plt.show()
 
     return
 
